078cbe6d28156fcb15f940049c65a318fb18936c
jqnfxa <ivborisov@stud.etu.ru>
2025-12-02 18:27:22 +0300

    [update]: minor speedup

--- task2/core/filter.cpp
+++ task2/core/filter.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <sstream>
 #include <unordered_set>
+#include <charconv>
 
 #include "filter.hpp"
 #include "hash.hpp"
@@ -9,13 +10,25 @@
 
 namespace task2
 {
+    inline const char *skip_whitespace(const char *ptr, const char *end)
+    {
+        while (ptr != end && std::isspace(*ptr))
+        {
+            ++ptr;
+        }
+
+        return ptr;
+    }
+
     void filter_duplicates(std::istream &input, std::ostream &output)
     {
         std::unordered_set<geometry::Point3, hash::Point3Hash> seen;
-        seen.reserve(1000);
+        seen.reserve(1024 * 64);
 
         std::string line;
-        geometry::Point3 temp_point{};
+        line.reserve(64);
+
+        geometry::Point3 point{};
 
         while (std::getline(input, line))
         {
@@ -24,13 +37,38 @@ namespace task2
                 continue;
             }
 
-            std::stringstream ss(line);
-            if (ss >> temp_point.x >> temp_point.y >> temp_point.z)
+            const char *ptr = line.data();
+            const char *end = line.data() + line.size();
+
+            /* X */
+            ptr = skip_whitespace(ptr, end);
+            auto res = std::from_chars(ptr, end, point.x);
+            if (res.ec != std::errc())
             {
-                if (seen.insert(temp_point).second)
-                {
-                    output << line << '\n';
-                }
+                continue;
+            }
+            ptr = res.ptr;
+
+            /* Y */
+            ptr = skip_whitespace(ptr, end);
+            res = std::from_chars(ptr, end, point.y);
+            if (res.ec != std::errc())
+            {
+                continue;
+            }
+            ptr = res.ptr;
+
+            /* Z */
+            ptr = skip_whitespace(ptr, end);
+            res = std::from_chars(ptr, end, point.z);
+            if (res.ec != std::errc())
+            {
+                continue;
+            }
+
+            if (seen.insert(point).second)
+            {
+                output << line << '\n';
             }
         }
     }
